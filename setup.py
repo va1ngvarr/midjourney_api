ae9dfa252480d11cf5618c02b0416e3cdc250915
Igor Vasiljev <93223722+va1ngvarr@users.noreply.github.com>
2023-06-14 01:09:36 +0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="midjourney_api",
+    version="1.0",
+    license="MIT",
+    author="Igor Vasilev",
+    author_email="va1ngvarr@gmail.com",
+    packages=find_packages("midjourney"),
+    package_dir={"": "midjourney"},
+    url="https://github.com/va1ngvarr/midjourney_api",
+    keywords="midjourney neuronetwork ml ml_python",
+    install_requires=["requests", "pandas"],
+)
